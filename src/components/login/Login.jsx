253a7438a758edb48f54a6c4ceb7c447ccc84dee
creator-krixus <wilsonrueda2@gmail.com>
2021-11-13 00:17:05 -0500

    first commit

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Login() {
+    return (
+        <div className="col-auto text-center p-3">
+            <h1>Ingresa con tus credencialess</h1>
+        </div>
+    )
+}
